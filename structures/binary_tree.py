d910d9b2e786835a25254a57f74c6442451bc772
SvetlanaSumets11 <svetasumets@gmail.com>
2021-06-01 19:36:25 +0300

    Commit structures 2

--- structures/binary_tree.py
+++ structures/binary_tree.py
@@ -1,14 +1,14 @@
 """This module is for implementing a binary tree"""
 
 class BinaryTree:
-	"""This binary tree class"""
+    """This binary tree class"""
     def __init__(self, value):
         self.left = None
         self.right = None
         self.value = value
 
     def add(self, value):
-    	"""Adding a node to a tree"""
+        """Adding a node to a tree"""
         if value < self.value:
             if self.left is None:
                 self.left = BinaryTree(value)
@@ -21,17 +21,46 @@ class BinaryTree:
                 self.right.add(value)
 
     def find(self, value):
-    	"""Method for finding a node in a tree"""
+        """Method for finding a node in a tree"""
         if value < self.value:
             if self.left is None:
                 return False
             return self.left.find(value)
-        elif value > self.value:
+        if value > self.value:
             if self.right is None:
                 return False
             return self.right.find(value)
-        else:
+        if value == self.value:
             return True
+
+    def find_min(self, node):
+        """helper function to remove a node from a tree"""
+        return self.find_min(node.left) if node.left is not None else node
+
+    def delete(self, value, node = None):
+        """function to remove a node from a tree"""
+        if not self.find(value):
+            raise ValueError("Нет узла с таким значением")
+        if node is None:
+            node = self
+        if node is None:
+            return None
+        if value < node.value:
+            node.left = self.delete(value, node.left)
+            return node
+        if value > node.value:
+            node.right = self.delete(value, node.right)
+            return node
+
+        if node.left is None:
+            return node.right
+        if node.right is None:
+            return node.left
+        if node.left is not None and node.right is not None:
+            min_value = self.find_min(node.right).value
+            node.value = min_value
+            node.right = self.delete(min_value, node.right)
+            return node
 
     def __str__(self, level=0, unit=''):
         res = ""
@@ -52,3 +81,5 @@ class BinaryTree:
 # print(tree)
 # print(tree.find(7))
 # print(tree.find(14))
+# tree.delete(35)
+# print(tree)
--- structures/hash_table.py
+++ structures/hash_table.py
@@ -5,7 +5,7 @@ from linked_list import LinkedList
 class LinkedListHash(LinkedList):
     """This is the linked list class for the hash table"""
 
-    def is_value(self, key):
+    def this_value(self, key):
         """Method for checking the existence of a node with a given value in the list"""
         current = self.head
         while current is not None:
@@ -13,6 +13,21 @@ class LinkedListHash(LinkedList):
                 return current.value[1]
             current = current.next
         return None
+
+    def delete_value(self, key):
+        """Method for removing a node / nodes by a given key from the list"""
+        if self.is_empty():
+            print("Список пуст")
+        else:
+            current = self.head
+            ind = 0
+
+            while current is not None:
+                if current.value[0] == key:
+                    self.delete(ind)
+                else:
+                    ind += 1
+                current = current.next
 
 
 class HashTable:
@@ -26,7 +41,7 @@ class HashTable:
         return sum(ord(item) for item in string) % self.size
 
     def __getitem__(self, string):
-        return self.table[self.hash_code(string)].is_value(string)
+        return self.table[self.hash_code(string)].this_value(string)
 
     def __setitem__(self, string, value):
         if self[string] is None:
@@ -37,10 +52,31 @@ class HashTable:
         for item in self.table:
             print(str(item))
 
+    def delete(self, string):
+        """Delete node by key"""
+        if self[string] is not None:
+            self.table[self.hash_code(string)].delete_value(string)
+
+    def find(self, string):
+        """Find node by key"""
+        return self[string] is not None
+
+    def add(self, string, value):
+        """Add node"""
+        self[string] = value
+
 
 # has = HashTable(20)
 # has["Hello"] = 5
 # has["eHllo"] = 5
 # has["My_world"] = 1
+# has["My love"] = 9
 # print(has.hash_code("Hello"))
 # has.print_table()
+# print(has["eHllo"])
+# print(has.find("eHllo"))
+# has.delete("eHllo")
+# print()
+# has.print_table()
+# print(has.find("eHllo"))
+# print(has["eHllo"])
